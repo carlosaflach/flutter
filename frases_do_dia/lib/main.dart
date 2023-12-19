ee0fc1e6633ea47a1ad67b101cc7f40c896d6139
Carlos Afonso <carlos.flach15@gmail.com>
2023-12-19 22:11:40 +0100

    feat: finish the interface of app

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -20,18 +20,42 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text('Frases do dia', style: TextStyle()),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        decoration:
-            BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),
-        child: Column(
-          children: [],
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Frases do dia', style: TextStyle()),
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            // Define a largura como 100% do espaço em tela.
+            padding: EdgeInsets.all(16),
+            // decoration: BoxDecoration(
+            //     border: Border.all(width: 3, color: Colors.amber)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("images/logo.png"),
+                Text(
+                  "Clique abaixo para gerar uma frase",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.green)),
+                  child: Text(
+                    "Nova Frase",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
