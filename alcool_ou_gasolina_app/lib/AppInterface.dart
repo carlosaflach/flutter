17313ad1ec5102752bdc2e5b65b5317788177545
Carlos Afonso <carlos.flach@momomedical.com>
2023-12-21 11:23:07 +0100

    feat: create the app interface

---
+++ alcool_ou_gasolina_app/lib/AppInterface.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class Interface extends StatefulWidget {
+  const Interface({super.key});
+
+  @override
+  State<Interface> createState() => _InterfaceState();
+}
+
+class _InterfaceState extends State<Interface> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Álcool ou Gasolina",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            Padding(padding: EdgeInsets.only(bottom: 32)),
+            Text(
+              "Saiba qual a melhor opcão para abastecimento do seu carro",
+              textAlign: TextAlign.left,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 32)),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(label: Text("Preco Álcool, ex: 1.59")),
+              style: TextStyle(fontSize: 22),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(label: Text("Preco Gasolina, ex: 3.15")),
+              style: TextStyle(fontSize: 22),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 16)),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Calcular",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateColor.resolveWith((states) => Colors.blue),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                    padding: MaterialStateProperty.resolveWith(
+                        (states) => (EdgeInsets.all(15))))),
+          ],
+        ),
+      ),
+    );
+  }
+}
